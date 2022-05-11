ae6d8af9a4d4864501c078c59ffd8588851da4df
Justin Lamb <justinlamb@me.com>
2022-05-11 11:18:09 -0700

    #child_gen implemented

--- knights_travails.rb
+++ knights_travails.rb
@@ -2,6 +2,10 @@
 
 # Handle locations on the chess board and creates structure based on moves.
 class Node
+  attr_accessor :current_point, :parent_node, :child_node
+
+  KNIGHT_MOVE_SET = [[-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1], [2, 1], [1, 2]].freeze
+
   def initialize(point = nil, parent = nil)
     @current_point = point
     @parent_node = parent
@@ -16,12 +20,24 @@ class Node
     end
     valid_points
   end
+
+  # From a given node, try all moves and return valid locations.
+  def child_gen(node)
+    point = node.current_point
+    possible_child_nodes = []
+    KNIGHT_MOVE_SET.each do |move|
+      possible_child_nodes << [point[0] + move[0], point[1] + move[1]]
+    end
+    validator(possible_child_nodes)
+  end
 end
 
 def knight_moves(start_point, end_point)
-  print 'Invalid location(s). Must be between 1-8' unless Node.new.validator([start_point, end_point]).length == 2
-  print 'No moves required.' if start_point == end_point
+  return print 'Invalid location. Must be between 1-8' unless Node.new.validator([start_point, end_point]).length == 2
+  return print 'No moves required.' if start_point == end_point
+
   start = Node.new(start_point)
+  p Node.new.child_gen(start)
 end
 
-knight_moves([3, 3], [3, 3])
+knight_moves([3, 8], [3, 4])
